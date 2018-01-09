3167c8f6b37f71797c9436a34ba83ad9274d2663
James Shapiro <jamesshapiro@gmail.com>
2018-01-09 07:08:00 -0800

    clean up scraper for measuring NFL talent production by conference

--- draft.py
+++ draft.py
@@ -7,31 +7,46 @@ import json
 from unidecode import unidecode
 import bs4
 import sys
+import maya
 
 ACC = ['Boston College', 'Clemson', 'Florida State', 'Louisville', 'North Carolina State', 'NC State', 'Syracuse', 'Wake Forest', 'Duke', 'Georgia Tech', 'Miami', 'Miami (FL)', 'North Carolina', 'Pittsburgh', 'Virginia', 'Virginia Tech']
-Big12 = ['Baylor', 'Iowa State', 'Kansas', 'Kansas State', 'Oklahoma', 'Oklahoma State', 'TCU', 'Texas', 'Texas Tech', 'West Virginia']
-BigTen = ['Illinois', 'Indiana', 'Iowa', 'Maryland', 'Michigan', 'Michigan State', 'Minnesota', 'Nebraska', 'Northwestern', 'Ohio State', 'Penn State', 'Purdue', 'Rutgers', 'Wisconsin']
-Pac12 = ['Arizona', 'Arizona State', 'California', 'UCLA', 'Colorado', 'Oregon', 'Oregon State', 'USC', 'Stanford', 'Utah', 'Washington', 'Washington State']
+BIG_12 = ['Baylor', 'Iowa State', 'Kansas', 'Kansas State', 'Oklahoma', 'Oklahoma State', 'TCU', 'Texas', 'Texas Tech', 'West Virginia']
+BIG_TEN = ['Illinois', 'Indiana', 'Iowa', 'Maryland', 'Michigan', 'Michigan State', 'Minnesota', 'Nebraska', 'Northwestern', 'Ohio State', 'Penn State', 'Purdue', 'Rutgers', 'Wisconsin']
+PAC_12 = ['Arizona', 'Arizona State', 'California', 'UCLA', 'Colorado', 'Oregon', 'Oregon State', 'USC', 'Stanford', 'Utah', 'Washington', 'Washington State']
 SEC = ['Alabama', 'Arkansas', 'Auburn', 'Florida', 'Georgia', 'Kentucky', 'LSU', 'Mississippi', 'Ole Miss', 'Mississippi State', 'Missouri', 'South Carolina', 'Tennessee', 'Texas A&M', 'Texas A&amp;M', 'Vanderbilt']
-all_confs = ACC[:]
-all_confs.extend(Big12)
-all_confs.extend(BigTen)
-all_confs.extend(Pac12)
-all_confs.extend(SEC)
 
-only_tally_first_rounders = True
+POWER_5 = ACC + BIG_12 + BIG_TEN + PAC_12 + SEC
+
+only_tally_first_rounders = False
+last_five_years = True
+CURRENT_YEAR = maya.now().datetime().year
 SINCE_BCS = 1999
-LAST_FIVE_YEARS = 2013
-LAST_TEN_YEARS = 2008
+LAST_FIVE_YEARS = CURRENT_YEAR - 5
+LAST_TEN_YEARS = CURRENT_YEAR - 10
 url_template = 'https://en.wikipedia.org/wiki/{}_NFL_Draft'
 
-years = [url_template.format(year) for year in range(2013, 2018)]
+years = [url_template.format(year) for year in range(SINCE_BCS, CURRENT_YEAR)]
+if last_five_years:
+    years = [url_template.format(year) for year in range(LAST_FIVE_YEARS, CURRENT_YEAR)]
 
 def is_draft_pick_row(row):
     return '<span id="Pick_' in str(row)
 
+def school_to_conference(school):
+    if school in ACC:
+        return 'ACC'
+    elif school in BIG_12:
+        return 'Big 12'
+    elif school in BIG_TEN:
+        return 'Big Ten'
+    elif school in PAC_12:
+        return 'Pac 12'
+    else:
+        return 'SEC'
+    
+
 def tally_drafts(urls):
-    school_counts = []
+    schools = []
     for url in urls:
         print('downloading... ' + url)
         draft_picks_raw_html = unidecode(requests.get(url).text)
@@ -42,40 +57,19 @@ def tally_drafts(urls):
         for t in tags:
             l = [child for child in t.children if child != u'\n']
             result = list(re.split(r'[<>]', str(l[1])))
-            if len(result) == 5:
-                draft_round = result[2]
-            else:
-                draft_round = result[4]
+            draft_round = result[len(result) // 2]
             if only_tally_first_rounders and draft_round != "1":
                 continue
             try:
                 school = re.split(r'[<>]', str(l[6]))[4]
-                if school in all_confs:
-                    school_counts.append(school)
+                if school in POWER_5:
+                    schools.append(school)
             except IndexError:
                 pass
-    
-    c = collections.Counter(school_counts)
-    schools_by_count = []
-    for school in c:
-        schools_by_count.append((school, c[school]))
-    conf_counts = []
-    for school in schools_by_count:
-        school_name = school[0]
-        if school_name in ACC:
-            school_conf = 'ACC'
-        elif school_name in Big12:
-            school_conf = 'Big 12'
-        elif school_name in BigTen:
-            school_conf = 'Big Ten'
-        elif school_name in Pac12:
-            school_conf = 'Pac 12'
-        else:
-            school_conf = 'SEC'
-        num_draft_picks = school[1]
-        conf_counts.extend([school_conf] * num_draft_picks)
-    c = collections.Counter(conf_counts)
-    total = sum([c['SEC'], c['ACC'], c['Pac-12'], c['Big Ten'], c['Big 12']])
+        
+    conferences = map(school_to_conference, schools)
+    c = collections.Counter(conferences)
+    total = len(conferences)
     for school in c:
         percent = format(100 * c[school] / total, '.1f')
         result = '{}: {}% ({}/{})'.format(school, percent, c[school], total)
