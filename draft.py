e930b3c50c011ca241832541e0d142b5cb0b13c2
James Shapiro <jamesshapiro@gmail.com>
2018-01-10 08:40:18 -0800

    fix a bug in tallying script and make it more usable

--- draft.py
+++ draft.py
@@ -8,6 +8,7 @@ from unidecode import unidecode
 import bs4
 import sys
 import maya
+import os.path
 
 ACC = ['Boston College', 'Clemson', 'Florida State', 'Louisville', 'North Carolina State', 'NC State', 'Syracuse', 'Wake Forest', 'Duke', 'Georgia Tech', 'Miami', 'Miami (FL)', 'North Carolina', 'Pittsburgh', 'Virginia', 'Virginia Tech']
 BIG_12 = ['Baylor', 'Iowa State', 'Kansas', 'Kansas State', 'Oklahoma', 'Oklahoma State', 'TCU', 'Texas', 'Texas Tech', 'West Virginia']
@@ -17,20 +18,41 @@ SEC = ['Alabama', 'Arkansas', 'Auburn', 'Florida', 'Georgia', 'Kentucky', 'LSU',
 
 POWER_5 = ACC + BIG_12 + BIG_TEN + PAC_12 + SEC
 
-only_tally_first_rounders = False
 last_five_years = True
 CURRENT_YEAR = maya.now().datetime().year
-SINCE_BCS = 1999
-LAST_FIVE_YEARS = CURRENT_YEAR - 5
-LAST_TEN_YEARS = CURRENT_YEAR - 10
+FIRST_BCS_DRAFT_YEAR = 1999
+FIVE_YEARS_AGO = CURRENT_YEAR - 5
+TEN_YEARS_AGO = CURRENT_YEAR - 10
 url_template = 'https://en.wikipedia.org/wiki/{}_NFL_Draft'
-
-years = [url_template.format(year) for year in range(SINCE_BCS, CURRENT_YEAR)]
+START_YEAR = FIRST_BCS_DRAFT_YEAR
 if last_five_years:
-    years = [url_template.format(year) for year in range(LAST_FIVE_YEARS, CURRENT_YEAR)]
+    START_YEAR = FIVE_YEARS_AGO
+
+years = range(START_YEAR, CURRENT_YEAR)
 
 def is_draft_pick_row(row):
     return '<span id="Pick_' in str(row)
+
+def get_html(url, year):
+    if not os.path.isfile('./{}'.format(year)):
+        print('downloading... ' + url)
+        text = unidecode(requests.get(url).text)
+        with open('./{}'.format(year), 'w') as f:
+            f.write(text)
+    else:
+        print('reading... ' + url)
+        with open('./{}'.format(year), 'r') as f:
+            text = f.read()
+    return text
+
+def tally_draft_picks(tally, message):
+    c = collections.Counter(tally)
+    total = len(list(tally))
+    print(message)
+    for school in c:
+        percent = format(100 * c[school] / total, '.1f')
+        result = '{}: {}% ({}/{})'.format(school, percent, c[school], total)
+        print(result)
 
 def school_to_conference(school):
     if school in ACC:
@@ -43,13 +65,13 @@ def school_to_conference(school):
         return 'Pac 12'
     else:
         return 'SEC'
-    
 
-def tally_drafts(urls):
+def tally_drafts(years):
+    urls = [url_template.format(year) for year in range(START_YEAR, CURRENT_YEAR)]
+    first_round_schools = []
     schools = []
-    for url in urls:
-        print('downloading... ' + url)
-        draft_picks_raw_html = unidecode(requests.get(url).text)
+    for url, year in zip(urls, years):
+        draft_picks_raw_html = get_html(url, year)
         soup = bs4.BeautifulSoup(draft_picks_raw_html, "html.parser")
         tag = 'tr'
         tags = soup.find_all(tag)
@@ -57,22 +79,22 @@ def tally_drafts(urls):
         for t in tags:
             l = [child for child in t.children if child != u'\n']
             result = list(re.split(r'[<>]', str(l[1])))
-            draft_round = result[len(result) // 2]
-            if only_tally_first_rounders and draft_round != "1":
-                continue
+            if len(result) == 5 or result[1] == 'th':
+                draft_round = result[2]
+            else:
+                draft_round = result[4]
             try:
                 school = re.split(r'[<>]', str(l[6]))[4]
                 if school in POWER_5:
                     schools.append(school)
+                    if draft_round == "1":
+                        first_round_schools.append(school)
             except IndexError:
                 pass
         
-    conferences = map(school_to_conference, schools)
-    c = collections.Counter(conferences)
-    total = len(conferences)
-    for school in c:
-        percent = format(100 * c[school] / total, '.1f')
-        result = '{}: {}% ({}/{})'.format(school, percent, c[school], total)
-        print(result)
-    
+    draft_picks_by_conf = list(map(school_to_conference, schools))
+    first_rounders = list(map(school_to_conference, first_round_schools))
+    tally_draft_picks(draft_picks_by_conf, "ALL POWER-5 PICKS SINCE {}".format(START_YEAR))
+    tally_draft_picks(first_rounders, "ALL POWER-5 FIRST ROUND PICKS SINCE {}".format(START_YEAR))
+     
 tally_drafts(years)
