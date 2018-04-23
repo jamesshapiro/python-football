c29a58ee884ccbeb7a57a35c8e531f25dde303fb
James Shapiro <jamesshapiro@gmail.com>
2018-04-23 01:28:42 -0700

    Simplify logic for tallying how many draft picks came from each school/power-5 conference by fully leveraging pandas built-in functionality.

--- draft.py
+++ draft.py
@@ -9,25 +9,40 @@ Options:
   --lastxyears NUM_YEARS  compute for last X years [default: -1]
 """
 
-import collections
-import sys
-import maya
-import os.path
-from docopt import docopt
 import pandas as pd
+import collections
+import os
+import sys
+
+from docopt import docopt
 
 arguments = docopt(__doc__)
+
+YEARS = range(2009,2018)
+url_template = 'https://en.wikipedia.org/wiki/{}_NFL_Draft'
+START_YEAR = 1999
+CURRENT_YEAR = 2018
+years = range(START_YEAR, CURRENT_YEAR)
+urls = [url_template.format(year) for year in range(START_YEAR, CURRENT_YEAR)]
 
 ACC = ['Boston College', 'Clemson', 'Florida State', 'Louisville', 'North Carolina State', 'NC State', 'Syracuse', 'Wake Forest', 'Duke', 'Georgia Tech', 'Miami', 'Miami (FL)', 'North Carolina', 'Pittsburgh', 'Virginia', 'Virginia Tech']
 BIG_12 = ['Baylor', 'Iowa State', 'Kansas', 'Kansas State', 'Oklahoma', 'Oklahoma State', 'TCU', 'Texas', 'Texas Tech', 'West Virginia']
 BIG_TEN = ['Illinois', 'Indiana', 'Iowa', 'Maryland', 'Michigan', 'Michigan State', 'Minnesota', 'Nebraska', 'Northwestern', 'Ohio State', 'Penn State', 'Purdue', 'Rutgers', 'Wisconsin']
 PAC_12 = ['Arizona', 'Arizona State', 'California', 'UCLA', 'Colorado', 'Oregon', 'Oregon State', 'USC', 'Stanford', 'Utah', 'Washington', 'Washington State']
 SEC = ['Alabama', 'Arkansas', 'Auburn', 'Florida', 'Georgia', 'Kentucky', 'LSU', 'Mississippi', 'Ole Miss', 'Mississippi State', 'Missouri', 'South Carolina', 'Tennessee', 'Texas A&M', 'Texas A&amp;M', 'Vanderbilt']
-POWER_FIVE_DICT = {'ACC': ACC, 'BIG_12': BIG_12, 'BIG_TEN': BIG_TEN, 'PAC_12': PAC_12, 'SEC': SEC}
-SCHOOL_TO_CONF = collections.defaultdict(lambda: 'NOT POWER FIVE')
+POWER_FIVE_DICT = {'ACC': ACC, 'BIG 12': BIG_12, 'BIG TEN': BIG_TEN, 'PAC 12': PAC_12, 'SEC': SEC}
+SCHOOL_TO_CONF = collections.defaultdict(lambda: 'OTHER')
 for (conference, schools) in POWER_FIVE_DICT.items():
     SCHOOL_TO_CONF.update({school: conference for school in schools})
-        
+    
+def get_index_of_target_table(data):
+    for table_index in range(len(data)):
+        players = data[table_index]
+        if 'Player' in players.columns:
+            return table_index
+    print("NO PLAYER TABLE")
+    sys.exit(1)    
+    
 def get_data(url, year):
     if not os.path.isfile('./{}'.format(year)):
         print('downloading... ' + url)
@@ -39,57 +54,35 @@ def get_data(url, year):
     players = pd.read_csv('./{}'.format(year))
     return players
 
-def tally_draft_picks(tally, message):
-    c = collections.Counter(tally)
-    total = len(list(tally))
-    print(message)
-    idx = 0
-    for school in c.most_common():
-        percent = format(100 * c[school[0]] / total, '.1f')
-        result = '{}: {}% ({}/{})'.format(school[0], percent, c[school[0]], total)
-        print(result)
-        idx += 1
-        if idx == 10:
-            break
-    print()
+dfs = []
+for (url,year) in zip(urls,years):
+    df = get_data(url, year)
+    df['Year'] = df['Player'].apply(lambda x: year)
+    cols = df.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    df = df.reindex(columns=(['Year'] + list([a for a in df.columns if a != 'Year'])))
+    dfs.append(df)
+def fix_name(name):
+    if not name.replace(',','').replace(' ','').isalpha():
+        name = name[:-2]
+    return name[len(name)//2 + 1:]
 
-def school_to_conference(school):
-    return SCHOOL_TO_CONF[school]
-    
-def get_index_of_target_table(data):
-    for table_index in range(len(data)):
-        players = data[table_index]
-        if 'Player' in players.columns:
-            return table_index
-    print("NO PLAYER TABLE")
-    sys.exit(1)
+players = pd.concat(dfs)
+players['Name'] = players['Player'].apply(fix_name)
+players['Power 5'] = players['College'].apply(lambda college: SCHOOL_TO_CONF[college])
 
-def tally_drafts(years):
-    urls = [url_template.format(year) for year in range(START_YEAR, CURRENT_YEAR)]
-    all_first_rounders = []
-    all_rounders = []
-    for (url,year) in zip(urls,years):
-        players = get_data(url, year)
-        players['Name'] = players['Player'].apply(lambda x: x[len(x)//2 + 1:])
-        first_rounders = players[players['Rnd.'] == '1']
-        all_first_rounders.extend(list(first_rounders['College']))
-        all_rounders.extend(list(players['College']))
-    tally_draft_picks(all_rounders, "ALL PICKS BY SCHOOL SINCE {}".format(START_YEAR))
-    tally_draft_picks(all_first_rounders, "ALL FIRST ROUND PICKS BY SCHOOL SINCE {}".format(START_YEAR))
-    draft_picks_by_conf = list(map(school_to_conference, all_rounders))
-    first_rounders_by_conf = list(map(school_to_conference, all_first_rounders))
-    tally_draft_picks(draft_picks_by_conf, "ALL PICKS SINCE {}".format(START_YEAR))
-    tally_draft_picks(first_rounders_by_conf, "ALL FIRST ROUND PICKS SINCE {}".format(START_YEAR))
-    power_five_all_picks = list(map(school_to_conference, filter(lambda school: SCHOOL_TO_CONF[school] != 'NOT POWER FIVE', all_rounders)))
-    power_five_first_rounders = list(map(school_to_conference, filter(lambda school: SCHOOL_TO_CONF[school] != 'NOT POWER FIVE', all_first_rounders)))
-    tally_draft_picks(power_five_all_picks, "ALL POWER FIVE PICKS SINCE {}".format(START_YEAR))
-    tally_draft_picks(power_five_first_rounders, "ALL POWER FIVE FIRST ROUND PICKS SINCE {}".format(START_YEAR))
-    
-if __name__ == '__main__':
-    CURRENT_YEAR = maya.now().datetime().year
-    url_template = 'https://en.wikipedia.org/wiki/{}_NFL_Draft'
-    START_YEAR = int(arguments['--from'])
-    if arguments['--lastxyears'] != '-1':
-        START_YEAR = CURRENT_YEAR - int(arguments['--lastxyears'])
-    years = range(START_YEAR, CURRENT_YEAR)
-    tally_drafts(years)
+def print_result(description, result):
+    print(description, '\n', result, '\n\n')
+
+print_result('ALL FIRST ROUND PICKS BY SCHOOL SINCE 1999', players[players['Rnd.'] == '1']['College'].value_counts().head(20))
+print_result('ALL PICKS BY SCHOOL SINCE 1999',
+             players['College'].value_counts().head(20))
+print_result('ALL FIRST ROUND PICKS SINCE 1999',
+             players[players['Rnd.'] == '1']['Power 5'].value_counts())
+print_result('ALL PICKS SINCE 1999',
+             players['Power 5'].value_counts())
+print_result('ALL POWER 5 FIRST ROUND PICKS SINCE 1999',
+             players[(players['Rnd.'] == '1') & (players['Power 5'] != 'OTHER')]['Power 5'].value_counts())
+print_result('ALL POWER 5 PICKS SINCE 1999',
+             players[players['Power 5'] != 'OTHER']['Power 5'].value_counts())
+
